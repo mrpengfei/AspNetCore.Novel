050ac6333583cadc5a6d23561bf1201b6f27aad0
shipengfei <shipengfei@daishu360.com>
2016-08-22 10:21:25 +0800

    更新项目

---
+++ src/Novel.Web/Startup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using Novel.Service.IService;
+using Novel.Service.Service;
+
+namespace Novel.Web
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddSingleton<INovelService, NovelService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            loggerFactory.AddConsole();
+            loggerFactory.AddNLog();
+            env.ConfigureNLog("log.xml");
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                //routes.MapAreaRoute("Admin", "Admin", "admin/{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute(name: "areaRoute",
+    template: "{area:exists}/{controller=Home}/{action=Index}");
+                routes.MapRoute(name: "default", template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
---
+++ src/Novel.Web/ViewModels/NovelModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Novel.Web.ViewModels
+{
+    /// <summary>
+    /// 小说
+    /// </summary>
+    public class NovelModel
+    {
+        /// <summary>
+        /// 设置或取得 小说Id
+        /// </summary>
+        /// 创建者：史鹏飞
+        /// 创建日期：2016/8/19 18:30
+        /// 修改者：
+        /// 修改时间：
+        /// ----------------------------------------------------------------------------------------
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 设置或取得 小说名称
+        /// </summary>
+        /// 创建者：史鹏飞
+        /// 创建日期：2016/8/19 18:30
+        /// 修改者：
+        /// 修改时间：
+        /// ----------------------------------------------------------------------------------------
+        public string Name { get; set; }
+    }
+}
